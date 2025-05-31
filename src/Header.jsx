0862d85a79334584feef06c319527e7472aae304
Juan <juancsds@live.com>
2025-05-31 12:15:48 -0300

    Novos ajustes

--- src/Header.jsx
+++ src/Header.jsx
@@ -11,20 +11,15 @@ function Header() {
   };
 
   return (
-    <header style={{
-      background: '#bfa084',
-      padding: '20px 0',
-      marginBottom: '40px',
-      boxShadow: '0 2px 8px rgba(0,0,0,0.05)'
-    }}>
-      <div style={{
+    <header className='header'>
+      <div className='div-header' style={{
         display: 'flex',
         justifyContent: 'space-between',
         alignItems: 'center',
         padding: '0 20px'
       }}>
         <div style={{
-          color: '#fff',
+          color: '#666',
           fontWeight: 'bold',
           fontSize: '24px'
         }}>
@@ -40,21 +35,22 @@ function Header() {
             position: menuAberto ? 'absolute' : undefined,
             top: menuAberto ? '60px' : undefined,
             right: menuAberto ? '20px' : undefined,
-            backgroundColor: menuAberto ? '#bfa084' : undefined,
+            backgroundColor: menuAberto ? '#e5dede' : undefined,
+          
             padding: menuAberto ? '10px' : undefined,
             borderRadius: menuAberto ? '8px' : undefined,
             gap: menuAberto ? '20px' : '40px',
             zIndex: menuAberto ? 1000 : undefined,
           }}
         >
-          <Link to="/" style={{ color: '#fff', fontWeight: 'bold', textDecoration: 'none', fontSize: '20px' }}>Início</Link>
-          <Link to="/quem-sou-eu" style={{ color: '#fff', fontWeight: 'bold', textDecoration: 'none', fontSize: '20px' }}>Quem Sou Eu?</Link>
-          <Link to="/contato" style={{ color: '#fff', fontWeight: 'bold', textDecoration: 'none', fontSize: '20px' }}>Contato</Link>
+          <Link to="/" style={{ color: '#666', fontWeight: 'bold', textDecoration: 'none', fontSize: '20px' }}>Serviços</Link>
+          <Link to="/quem-sou-eu" style={{ color: '#666', fontWeight: 'bold', textDecoration: 'none', fontSize: '20px' }}>Quem Sou Eu?</Link>
+          <Link to="/contato" style={{ color: '#666', fontWeight: 'bold', textDecoration: 'none', fontSize: '20px' }}>Contato</Link>
         </nav>
 
         {/* Ícone de menu para mobile */}
         <div className="menu-mobile" style={{
-          color: '#fff',
+          color: '#666',
           cursor: 'pointer',
           //display: 'block' // mostramos o ícone, o CSS já cuida do display real por media query
         }} onClick={toggleMenu}>
